7be4a2c5462f57394faf84e1725cba6018076f3d
李志军 <lizhijun@lizhijundeMacBook-Pro.local>
2016-05-18 23:59:47 +0800

    初始化

---
+++ github/github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github
+//
+//  Created by 李志军 on 16/5/18.
+//  Copyright © 2016年 李志军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ github/github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  github
+//
+//  Created by 李志军 on 16/5/18.
+//  Copyright © 2016年 李志军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
